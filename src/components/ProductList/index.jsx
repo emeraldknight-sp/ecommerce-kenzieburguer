3b459de6c342aec319501175481cbf383aadbab2
David Almeida <contato.davidalmeida@outlook.com>
2022-12-15 17:32:35 -0300

    ⚡️  Improve performance

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,14 +1,1 @@
-import Product from "../Product";
-import "./styles.css";
-
-const ProductList = ({ products, showProducts, handleClick }) => {
-  return (
-    <section className="section">
-      {products.map((product) => (
-        <Product key={product.id} product={product} handleClick={handleClick} />
-      ))}
-    </section>
-  );
-};
-
-export default ProductList;
+export * from "./ProductList"
