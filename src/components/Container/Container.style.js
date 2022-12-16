b8fcb36992e5b0ccd42a76612baf7e02fbf13b2b
David Almeida <contato.davidalmeida@outlook.com>
2022-12-16 19:53:33 -0300

    💄 Add or update the UI and style files

--- src/components/Container/Container.style.js
+++ src/components/Container/Container.style.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.section`
+  display: flex;
+  flex-direction: column;
   max-width: 80vw;
   margin: 0 auto;
 `;
--- src/components/Main/Main.style.js
+++ src/components/Main/Main.style.js
@@ -1,10 +1,5 @@
 import styled from "styled-components";
 
 export const StyledMain = styled.main`
-  section {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    padding: 20px 0;
-  }
+  padding: 76px 0 0;
 `;
