34e7ff7373672a772734f749017f7ecbc80526d4
David Almeida <contato.davidalmeida@outlook.com>
2022-12-17 12:11:40 -0300

    💄 Add or update the UI or style files

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,12 +1,19 @@
+import { Container } from "../Container";
 import { ProductCard } from "../ProductCard/ProductCard";
 import "./styles.css";
 
 export const ProductList = ({ products, handleClick }) => {
   return (
     <div className="product-list">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} handleClick={handleClick} />
-      ))}
+      <Container>
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            handleClick={handleClick}
+          />
+        ))}
+      </Container>
     </div>
   );
 };
