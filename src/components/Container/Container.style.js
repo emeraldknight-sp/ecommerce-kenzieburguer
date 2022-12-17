cb52ffb6bec2be5d1112be7b54e86329cc437374
David Almeida <contato.davidalmeida@outlook.com>
2022-12-16 22:12:51 -0300

    ✅ Update: Container component

--- src/components/Container/Container.style.js
+++ src/components/Container/Container.style.js
@@ -3,6 +3,10 @@ import styled from "styled-components";
 export const StyledContainer = styled.section`
   display: flex;
   flex-direction: column;
-  max-width: 80vw;
+  max-width: 90vw;
   margin: 0 auto;
+
+  @media only screen and (min-width: 1024px) {
+    max-width: 80vw;
+  }
 `;
