b488eee087d979643ed0d1a12079a8f2733c919e
David Almeida <contato.davidalmeida@outlook.com>
2022-12-16 22:46:02 -0300

    🐛 Fix a bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 import { Header } from "./components/Header";
-import { Main } from "./components/Main/Main";
+import { Main } from "./components/Main";
 
 import { ProductList } from "./components/ProductList";
 import { Cart } from "./components/Cart";
