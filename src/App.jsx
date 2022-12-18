db9a0e9d6e42cb512a301e03d6dcd0f52df98fd2
David Almeida <contato.davidalmeida@outlook.com>
2022-12-18 10:25:37 -0300

    ♻️ Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Toaster } from "react-hot-toast";
-import { AppRoutes } from "./routes/routes";
+import { AppRoutes } from "./routes";
 import { GlobalStyle } from "./styles/global";
 
 export const App = () => {
