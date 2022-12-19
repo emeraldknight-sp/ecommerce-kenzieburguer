9b66a55052bc65c1f1ab9e7621e09f88890aa915
David Almeida <contato.davidalmeida@outlook.com>
2022-12-18 21:16:19 -0300

    ✅ Add: yup, react-hook-form and @hookform/resolvers

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,24 +1,41 @@
+import * as yup from "yup";
+import { toast } from "react-hot-toast";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import { Button } from "../../components/Button";
 import { Container } from "../../components/Container";
 import { StyledLogin } from "./Login.style";
 
 import KenzieBurguerBrand from "../../assets/logo.svg";
 import FastFood from "../../assets/fast-food-01.svg";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-hot-toast";
 
 export const Login = () => {
   const navigate = useNavigate();
 
-  const handleClick = (e, phrase) => {
-    e.preventDefault();
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Precisa ser um email válido")
+      .required("Campo obrigatório!"),
+    password: yup
+      .string()
+      .min(8, "Precisa ter no mínimo 8 caracteres")
+      .required("Campo obrigatório!"),
+    check: yup.boolean(),
+  });
 
-    toast.success(phrase, { id: phrase });
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-
+  const onSubmit = (data) => {
+    console.log(data);
     toast.success("Dados enviados!", { id: "data" });
   };
 
@@ -40,7 +57,8 @@ export const Login = () => {
               </figure>
             </button>
           </div>
-          <form className="form" onSubmit={(e) => onSubmit(e)}>
+          {/* FORMULARIO */}
+          <form className="form" onSubmit={handleSubmit(onSubmit)}>
             <div className="input-container">
               <label className="label" htmlFor="email">
                 Email
@@ -52,8 +70,11 @@ export const Login = () => {
                 name="email"
                 placeholder="Digite seu email ou telefone"
                 autoComplete="off"
+                {...register("email")}
               />
+              <span className="span error">{errors.email?.message}</span>
             </div>
+
             <div className="input-container">
               <label className="label" htmlFor="password">
                 Senha
@@ -64,16 +85,27 @@ export const Login = () => {
                 id="password"
                 name="password"
                 placeholder="Digite sua senha"
+                {...register("password")}
               />
+              <span className="span error">{errors.password?.message}</span>
             </div>
+
             <div className="checkbox">
               <label htmlFor="remember">Lembre-se de mim por 30 dias</label>
-              <input id="remember" type="checkbox" />
+              <input id="remember" type="checkbox" {...register("check")} />
             </div>
+
             <Button type="submit">Entrar</Button>
+
             <div className="span-group">
               <span className="span">
-                <a href="/login" onClick={(e) => handleClick(e, "Redefinir!")}>
+                <a
+                  href="/login"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    toast.success("Redefinir senha!", { id: "pwd" });
+                  }}
+                >
                   Esqueceu a senha?
                 </a>
               </span>
@@ -81,7 +113,10 @@ export const Login = () => {
                 Não possui conta?
                 <a
                   href="/login"
-                  onClick={(e) => handleClick(e, "Criar conta!")}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    toast.success("Criar conta!", { id: "signup" });
+                  }}
                 >
                   Crie uma agora!
                 </a>
