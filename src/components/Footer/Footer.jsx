cc1516625c1961b13fa2b18c31f5109e1d1d975b
David Almeida <contato.davidalmeida@outlook.com>
2022-12-19 20:54:56 -0300

    ✅ Add: Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,52 @@
+import { FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
+
+import { Container } from "../Container";
 import { StyledFooter } from "./Footer.style";
 
-export const Footer = ({ children }) => {
-  return <StyledFooter>{children}</StyledFooter>;
+export const Footer = () => {
+  return (
+    <StyledFooter>
+      <Container>
+        <div>
+          <div className="address">
+            <h3>Endereço</h3>
+            <p>160 NE. Durham Street Williamstown, NJ 08094</p>
+          </div>
+          <div className="links">
+            <h3>Contatos</h3>
+            <ul>
+              <li>
+                <a
+                  href="https://www.instagram.com/davidalmeidadev/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaInstagram size={16} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/davidalmeidadev/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaLinkedin size={16} />
+                </a>
+              </li>
+              <li>
+                <a href="/" target="_blank" rel="noopener noreferrer">
+                  <FaWhatsapp size={16} />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <span>
+          &copy;
+          {` ${new Date().getFullYear()}
+               All Rights Reserved`}
+        </span>
+      </Container>
+    </StyledFooter>
+  );
 };
--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -1,3 +1,58 @@
 import styled from "styled-components";
 
-export const StyledFooter = styled.footer``;
+export const StyledFooter = styled.footer`
+  background-color: var(--gray-1);
+  width: 100%;
+  padding: 10px 0;
+
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+  }
+
+  section > div {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+
+  h3 {
+    font-size: var(--content-md);
+    line-height: var(--line-height-md);
+    color: var(--gray-4);
+  }
+
+  p {
+    font-size: var(--content-xs);
+    line-height: var(--line-height-xs);
+    color: var(--gray-4);
+  }
+
+  span {
+    font-size: var(--content-xs);
+    line-height: var(--line-height-xs);
+    color: var(--gray-4);
+    margin: 0 auto;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 16px;
+  }
+
+  a {
+    color: var(--gray-4);
+  }
+
+  .address,
+  .links {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+  }
+`;
