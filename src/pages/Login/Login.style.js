8f777f9229ef47bd20b72641613f16b7e7e69100
David Almeida <contato.davidalmeida@outlook.com>
2022-12-27 00:53:49 -0300

    💄 Add or update the UI and style files

--- src/pages/Login/Login.style.js
+++ src/pages/Login/Login.style.js
@@ -8,6 +8,7 @@ export const StyledLogin = styled.div`
   height: 500px;
   min-height: 100vh;
   background-color: var(--gray-1);
+  padding: 50px 0 0;
 
   .login {
     display: flex;
@@ -22,85 +23,10 @@ export const StyledLogin = styled.div`
     cursor: pointer;
   }
 
-  .form {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    width: 270px;
-    padding: 10px;
-    border: 2px solid var(--transparent);
-    border-radius: 8px;
-
-    h2 {
-      font-size: var(--heading-2);
-      line-height: var(--lineheight-h2);
-      font-weight: bold;
-      color: var(--color-primary);
-      text-align: center;
-    }
-  }
-
-  .input-container {
-    display: flex;
-    flex-direction: column;
-    gap: 6px;
-  }
-
-  .label {
-    font-size: var(--content-xs);
-    line-height: var(--line-height-xs);
-  }
-
-  .input {
-    padding: 10px;
-    border: 1px solid var(--gray-3);
-    border-radius: 4px;
-    outline: none;
-  }
-
-  .checkbox {
-    display: flex;
-    flex-direction: row-reverse;
-    align-items: center;
-    justify-content: flex-end;
-    gap: 4px;
-
-    font-size: var(--content-xs);
-    line-height: var(--line-height-xs);
-
-    input {
-      margin: 0;
-    }
-  }
-
-  .span-group {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 12px;
-  }
-
-  .span {
-    display: flex;
-    font-size: var(--content-xs);
-    line-height: var(--line-height-xs);
-    gap: 4px;
-  }
-
-  .error {
-    color: var(--negative);
-  }
-
   .footer {
     width: 100%;
     background-color: var(--gray-3);
     color: var(--gray-1);
-  }
-
-  @media only screen and (min-width: 425px) {
-    .form {
-      width: 300px;
-    }
   }
 
   @media only screen and (min-width: 1024px) {
@@ -116,4 +42,18 @@ export const StyledLogin = styled.div`
       gap: 100px;
     }
   }
+`;
+
+export const StyledSpanGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const StyledSpan = styled.span`
+  display: flex;
+  font-size: var(--content-xs);
+  line-height: var(--line-height-xs);
+  gap: 4px;
 `;
--- src/pages/Register/Register.style.js
+++ src/pages/Register/Register.style.js
@@ -5,7 +5,10 @@ export const StyledRegister = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: 500px;
+  min-height: 100vh;
+  background-color: var(--gray-1);
+  padding: 50px 0 0;
 
   @media only screen and (min-width: 1024px) {
     section {
@@ -19,56 +22,6 @@ export const StyledRegister = styled.div`
     section {
       gap: 100px;
     }
-  }
-`;
-
-export const StyledFormCreateAccount = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-
-  width: 250px;
-  padding: 10px;
-  margin: 0 auto;
-  border: 2px solid var(--transparent);
-  border-radius: 8px;
-
-  a {
-    margin: 0 auto;
-  }
-
-  h2 {
-    font-size: var(--heading-2);
-    line-height: var(--lineheight-h2);
-    font-weight: bold;
-    color: var(--color-primary);
-    text-align: center;
-  }
-
-  input {
-    padding: 10px;
-    border: 1px solid var(--gray-3);
-    border-radius: 4px;
-    outline: none;
-  }
-
-  .checkbox {
-    display: flex;
-    flex-direction: row-reverse;
-    justify-content: flex-end;
-    align-items: center;
-    gap: 4px;
-
-    font-size: var(--content-xs);
-    line-height: var(--line-height-xs);
-
-    input {
-      margin: 0;
-    }
-  }
-
-  @media only screen and (min-width: 425px) {
-    width: 300px;
   }
 `;
 
